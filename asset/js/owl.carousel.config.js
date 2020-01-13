8e9b3a89a1018af149065024db3ee380e9b82598
Hossein Azizabadi Farahani <djvoltan@gmail.com>
2020-01-13 16:09:21 +0330

    Fix template

--- asset/js/owl.carousel.config.js
+++ asset/js/owl.carousel.config.js
@@ -11,7 +11,7 @@ $(document).ready(function($) {
         autoplayHoverPause:true,
         navText: ['<i class="owl-prev fa fa-angle-left"></i>', '<i class="owl-next fa fa-angle-right"></i>'],
         responsive:{
-            0:{items:3},
+            0:{items:2},
             600:{items:5},
             1000:{items:6}
         }
